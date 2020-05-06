2c04740fa09f6fa4564a77944f829b2a8128cca6
manohar <manumanohar1527@gmail.com>
2020-05-06 23:58:29 +0530

    Added eventListener to the startScreen

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const lives = document.querySelector('.lives');
 const gameArea = document.querySelector('.gameArea');
 const startScreen = document.querySelector('.startScreen');
 const gameMessage = document.querySelector('.gameMessage');
+
+//adding eventListener to the startScreen
+startScreen.addEventListener('click', start);
+
+
+//function which starts the gamePlay
+function start() {
+    startScreen.classList.add('hide');
+    container.classList.remove('hide');
+
+}
