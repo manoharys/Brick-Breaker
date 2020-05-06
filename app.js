a81d4ef3f52b7f33f48427a94bc55f3e531e1e63
manohar <manumanohar1527@gmail.com>
2020-05-06 23:48:18 +0530

    setting up structure of the game

---
+++ app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+const score = document.querySelector('.score');
+const lives = document.querySelector('.lives');
+const gameArea = document.querySelector('.gameArea');
+const startScreen = document.querySelector('.startScreen');
+const gameMessage = document.querySelector('.gameMessage');
